134ac5fe71214caa5b48a4409f446f4c8cc522bb
necros <necros83@gmail.com>
2023-12-24 22:20:18 +0700

    fix add

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,30 @@
 import os
+
+from dotenv import load_dotenv
 from selene import browser
 from selenium.webdriver.chrome.options import Options
 import pytest
 from selenium import webdriver
 from utils import attach
 
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+DEFAULT_BROWSER_VERSION = '100.0'
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
+    browser_version = browser.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
