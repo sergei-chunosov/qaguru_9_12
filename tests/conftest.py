931268ffe2e9526f356254a24dc8381b81d45295
necros <necros83@gmail.com>
2023-12-24 20:51:56 +0700

    prepare to homework

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+import os
+
+from selene import browser
+from selenium.webdriver.chrome.options import Options
+import pytest
+from selenium import webdriver
+from utils import attach
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}]:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+    browser.config.base_url = "https://demoqa.com/"
+    browser.config.window_width = '1900'
+    browser.config.window_height = '1080'
+    browser.config.timeout = 4
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
